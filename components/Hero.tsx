ff1156ccf07e864292c9370841e02bfbb98a65ad
Harshal <hraj1137@gmail.com>
2025-06-18 13:10:08 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,9 @@
+// components/Hero.tsx
 "use client";
 
 import React from "react";
-import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import PhoneMockup from "./PhoneMockup";
-import { BigText } from "./BigText";
+
 
 export const Hero: React.FC = () => {
   return (
@@ -40,28 +40,7 @@ export const Hero: React.FC = () => {
         </p>
       </div>
 
-      {/* ─── full-width Lottie bg behind the phone ─── */}
-      <div
-        className="
-          absolute inset-0
-          w-full h-full
-          pointer-events-none
-          z-10
-          overflow-hidden
-        "
-      >
-        <DotLottieReact
-          src="https://lottie.host/00a9c42d-c03e-481b-ab67-da824e084dc7/6umIz3PW3E.lottie"
-          loop
-          autoplay
-          speed={1}
-          className="w-full h-full object-cover"
-        />
-      </div>
-
-      {/* ─── BigText background ─── */}
-     
-
+    
       {/* ─── phone mockup ─── */}
       <div
         className="
